5df9cbd8f02899e3ed9daa420a7f5a19791cfe32
LandonHarter <landonharter@outlook.com>
2025-03-25 18:16:10 -0400

    fix up urls

--- src/lib/auth/client.ts
+++ src/lib/auth/client.ts
@@ -3,7 +3,8 @@
 import { createAuthClient } from "better-auth/react";
 
 const baseURL =
-	process.env.NEXT_PUBLIC_BASE_URL || "https://luminosity.azurewebsites.net";
+	process.env.NEXT_PUBLIC_BASE_URL ||
+	"https://luminosityai.azurewebsites.net";
 export const authClient = createAuthClient({
 	baseURL,
 });
--- src/trpc/client.ts
+++ src/trpc/client.ts
@@ -3,7 +3,8 @@ import superjson from "superjson";
 import { AppRouter } from "./router";
 
 const baseUrl =
-	process.env.NEXT_PUBLIC_BASE_URL || "https://luminosity.azurewebsites.net";
+	process.env.NEXT_PUBLIC_BASE_URL ||
+	"https://luminosityai.azurewebsites.net";
 export const trpcClient = createTRPCProxyClient<AppRouter>({
 	links: [
 		httpBatchLink({ url: `${baseUrl}/api/trpc` }),
