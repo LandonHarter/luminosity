063ef9eae16672775354130009e1e39e2d27c801
LandonHarter <landonharter@outlook.com>
2025-03-25 18:38:32 -0400

    max duration

--- src/app/(app)/dashboard/(home)/page.tsx
+++ src/app/(app)/dashboard/(home)/page.tsx
@@ -1,5 +1,6 @@
 import GenerateVideo from "./_components/generate";
 
+export const maxDuration = 1800;
 export default function DashboardHome() {
 	return (
 		<div className="flex h-full w-full flex-col items-center justify-center">
